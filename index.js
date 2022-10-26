fe3a323030597c01d71865a9eeb164f1a1e47ad5
JfJubaer <skk5727@gmail.com>
2022-10-26 22:12:55 +0600

    3rd

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const express = require('express')
+const express = require('express');
+var cors = require('cors')
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+
+const courses = require('./Data/courses.json');
 
 app.get('/', (req, res) => {
   res.send('Server of 007 is on the way....')
+})
+
+app.get('/courses',(req,res)=>{
+  res.send(courses);
+})
+app.get('/courses/:id',(req,res)=>{
+  const id = req.params.id;
+    const selectedCourses = courses.find(n => n._id === id);
+    res.send(selectedCourses);
 })
 
 app.listen(port, () => {
