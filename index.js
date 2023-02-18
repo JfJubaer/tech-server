ac060eeff8b689dba61783747b472937e0295247
JfJubaer <skk5727@gmail.com>
2023-02-19 02:19:25 +0600

    polished

--- index.js
+++ index.js
@@ -3,23 +3,28 @@ var cors = require('cors')
 const app = express();
 const port = process.env.PORT || 4000;
 
+
+// middle wire
 app.use(cors());
+app.use(express.json());
 
 const courses = require('./Data/courses.json');
 
+// api section
 app.get('/', (req, res) => {
   res.send('Only for Tech Learners....')
 })
 
-app.get('/courses',(req,res)=>{
+app.get('/courses', (req, res) => {
   res.send(courses);
 })
-app.get('/courses/:id',(req,res)=>{
+app.get('/courses/:id', (req, res) => {
   const id = req.params.id;
-    const selectedCourses = courses.find(c => c.id === id);
-    res.send(selectedCourses);
+  const selectedCourses = courses.find(c => c.id === id);
+  res.send(selectedCourses);
 })
 
+// server
 app.listen(port, () => {
   console.log(`i am server of Tech Learning site at port ${port}`)
 })
